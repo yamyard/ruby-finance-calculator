d59d954ab483b415fb02d1bd82d23fb9e99930a0
yamyard <yamyard@hotmail.com>
2025-08-19 18:05:53 +0800

    Update loan.rb

--- lib/loan.rb
+++ lib/loan.rb
@@ -1,17 +1,19 @@
 require 'bigdecimal'
 require 'bigdecimal/util'
 
-module Investment
-  def self.roi(final_value, initial_value)
-    final_value = final_value.to_d
-    initial_value = initial_value.to_d
-    (final_value - initial_value) / initial_value
+module Loan
+  def self.emi(principal, annual_rate, years)
+    principal = principal.to_d
+    monthly_rate = (annual_rate.to_d / 12)
+    months = years.to_i * 12
+    numerator = principal * monthly_rate * (1 + monthly_rate)**months
+    denominator = (1 + monthly_rate)**months - 1
+    numerator / denominator
   end
 
-  def self.cagr(final_value, initial_value, periods)
-    final_value = final_value.to_d
-    initial_value = initial_value.to_d
-    periods = periods.to_d
-    (final_value / initial_value)**(1/periods) - 1
+  def self.total_interest(principal, emi, years)
+    emi = emi.to_d
+    months = years.to_i * 12
+    emi * months - principal.to_d
   end
 end
